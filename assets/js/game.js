4e891c8365e09262ff316a157ec0e197db44864f
Jonathon Ashcraft <ashcraft.jonathon@gmail.com>
2022-01-04 18:29:33 -0700

    added fight loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,16 @@
+//player variables 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+//enemy variables
 var enemyHealth = 50;
 var enemyAttack = 12;
+var enemyNames = ["Roborto","Amy Android","Robo Trumble"]
 
-var fight = function() {
+
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
 
   var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -69,4 +69,6 @@ else {
 }
 
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
