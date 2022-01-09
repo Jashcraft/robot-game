7a3d62d1424582bf367d3c80482226ed3208c5b6
Jonathon Ashcraft <ashcraft.jonathon@gmail.com>
2022-01-09 16:44:10 -0700

    add RNJeezus

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,14 +24,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack -3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -50,7 +51,9 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      damage = randomNumber(enemyAttack -3, enemyAttack)
+      
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -66,7 +69,12 @@ var fight = function(enemyName) {
     }
   };
 
-  
+var randomNumber = function (min, max) {
+  var value= Math.floor(Math.random() * (max - min +1 ) + min); 
+
+  return value
+};
+
 var endGame = function(){
   // if player is still alive, player Wins! 
   if (playerHealth > 0) {
@@ -124,6 +132,7 @@ var shop = function() {
     case "leave":
       window.alert("Leaving the Store!");
       // does nothing ending the funciton 
+      break;
       default:
         window.alert("You did not pick a valid option. Try again.");
         shop();
@@ -148,7 +157,7 @@ for(var i = 0; i < enemyNames.length; i++) {
         var pickedEnemyName = enemyNames[i];
 
         // reset health before starting new fight 
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40,60);
 
         //use debugger to pause script from running and check whats going on 
         //debugger;
